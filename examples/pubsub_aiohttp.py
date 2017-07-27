47de7029d715f83b87edcbf5f5af44453c1b4570
Eric Appelt <eric.appelt@gmail.com>
2017-07-27 06:05:53 -0500

    Add pubsub example

---
+++ examples/pubsub_aiohttp.py
@@ -0,0 +1,54 @@
+import asyncio
+from aiohttp import web
+
+
+class Subscription():
+
+    def __init__(self, hub):
+        self.hub = hub
+        self.queue = asyncio.Queue()
+
+    def __enter__(self):
+        hub.subscriptions.add(self.queue)
+        return self.queue
+
+    def __exit__(self, type, value, traceback):
+        hub.subscriptions.remove(self.queue)
+
+
+class Hub():
+
+    def __init__(self):
+        self.subscriptions = set()
+
+    def publish(self, message):
+        for queue in self.subscriptions:
+            queue.put_nowait(message)
+
+
+hub = Hub()
+
+
+async def sub(request):
+    resp = web.StreamResponse()
+    resp.headers['content-type'] = 'text/plain'
+    resp.status_code = 200
+    await resp.prepare(request)
+    with Subscription(hub) as queue:
+        while True:
+            msg = await queue.get()
+            resp.write(bytes(f'{msg}\r\n', 'utf-8'))
+    return resp
+
+
+async def pub(request):
+    msg = request.query.get('msg', '')
+    hub.publish(msg)
+    return web.Response(text='ok')
+
+
+if __name__ == '__main__':
+    app = web.Application()
+    app.router.add_get('/', sub)
+    app.router.add_post('/', pub)
+    web.run_app(app)
